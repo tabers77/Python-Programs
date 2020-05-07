b805ae2ab3930d22dea0fbae2e087fa32dff037d
Carlos De La Cruz <58544751+tabers77@users.noreply.github.com>
2020-05-07 07:47:16 +0200

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -25,17 +25,11 @@ def get_password_leaks(hashes,hash_to_check):
 	return 0 
 #2
 def pwned_api_check (password):
-		#print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper()) # this will return the hashe version I find here: https://passwordsgenerator.net/sha1-hash-generator/
 		sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
 		first_5_ch, tail = sha1password[:5], sha1password[5:] #Here our password into 2 
 		response = request_api_data(first_5_ch)
-		#print(first_5_ch,tail)
-		#print(response) #we print the response to see the output
 		return get_password_leaks(response,tail) # here I am returning the password 
-		#return read_response(response)
-		#return sha1password
-#pwned_api_check('hello')
-#request_api_data('40BD0') # remember to convert to string  
+
 #4
 def main (args):
 	for password in args:
